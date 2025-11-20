517fbffaa72a931d5e77bf3f66b45ea97118a038
LuckyLukic <lucabjjiannice@gmail.com>
2025-11-20 21:43:14 +0100

    setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logoImage from "../assets/quiz-logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logoImage} alt="quiz logo" />
+      <h1>Quiz</h1>
+    </header>
+  );
+}
