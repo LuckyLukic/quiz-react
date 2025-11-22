36d012014f1e1add7c7bfddcc237c83a8c075ded
LuckyLukic <lucabjjiannice@gmail.com>
2025-11-22 10:25:12 +0100

    progress bar fixed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,31 +1,60 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../question";
+import quizCompleteImage from "../assets/quiz-complete.png";
+import ProgressTimer from "./ProgressTimer";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
 
-  function handleSelectedAnswer(answer) {
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectedAnswer = useCallback(function handleSelectedAnswer(
+    answer
+  ) {
     setUserAnswers((answerSelectedList) => {
       return [...answerSelectedList, answer];
     });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectedAnswer(null),
+    [handleSelectedAnswer]
+  );
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImage} alt="quiz complete logo" />
+        <h2>Quiz is completed</h2>
+      </div>
+    );
   }
 
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
-    <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((singleAnswer) => {
-          return (
-            <li key={singleAnswer} className="answer">
-              <button onClick={() => handleSelectedAnswer(singleAnswer)}>
-                {singleAnswer}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+    <div id="quiz">
+      <div id="question">
+        <ProgressTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={() => handleSelectedAnswer(null)}
+        ></ProgressTimer>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((singleAnswer) => {
+            return (
+              <li key={singleAnswer} className="answer">
+                <button onClick={handleSkipAnswer}>{singleAnswer}</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
